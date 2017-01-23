adb4f919f20446c132764eadcbde11f7aafe96e8
Munair Simpson <munair@gmail.com>
2017-01-23 11:52:29 +0900

    Initial commit

---
+++ js/capoeiralife.js
@@ -0,0 +1,19 @@
+var token = '3024617218.859f470.96f709da1d73415293c29ab0abb32e84',
+    hashtag='capoeiralife', 
+    num_photos = 1;
+ 
+$.ajax({
+	url: 'https://api.instagram.com/v1/tags/' + hashtag + '/media/recent',
+	dataType: 'jsonp',
+	type: 'GET',
+	data: {access_token: token, count: num_photos},
+	success: function(data){
+		console.log(data);
+		for(x in data.data){
+			$('instagram').append('<img src="'+data.data[x].images.standard_resolution.url+'">');  
+		}
+	},
+	error: function(data){
+		console.log(data);
+	}
+});
